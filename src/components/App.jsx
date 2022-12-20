95bc6f4b687b939308943c01c922b92f212503cb
Iryna Furhala <irynafurhala@gmail.com>
2022-12-20 15:00:55 +0200

    add sections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,39 @@
 import React from 'react'
 import { Buttons } from './Buttons/Buttons'
 import { Statistics } from './Statistics/Statistics'
+import { Section } from './Section'
 
 export class App extends React.Component {
   state = {
     good: 0,
-    bad: 0, 
+    bad: 0,
     neutral: 0
   }
 
   changeGoodCounter = () => {
-    this.setState({good: this.state.good+= 1})
+    this.setState({ good: this.state.good += 1 })
   }
   changeBadCounter = () => {
-    this.setState({bad: this.state.bad+= 1})
+    this.setState({ bad: this.state.bad += 1 })
   }
   changeNeutralCounter = () => {
-    this.setState({neutral: this.state.neutral+= 1})
+    this.setState({ neutral: this.state.neutral += 1 })
   }
   render() {
-  return <div>
-    <h1>Please leave feedback</h1>
-    <h1>Statistics</h1>
-    <Statistics state={this.state}/>
-    <Buttons addBadFeedback={this.changeBadCounter} 
-    addNeutralFeedback={this.changeNeutralCounter} 
-    addGoodFeedback={this.changeGoodCounter}/>
-  </div>
+    return <div>
+      <Section title={'Please leave feedback'}></Section>
+      {this.state.bad + this.state.good + this.state.neutral > 0 ?
+        <div>
+          <Section title="Statistics" />
+          <Statistics state={this.state} />
+        </div>
+        : <Section title='No feedbacks given' />
+
+      }
+
+      <Buttons addBadFeedback={this.changeBadCounter}
+        addNeutralFeedback={this.changeNeutralCounter}
+        addGoodFeedback={this.changeGoodCounter} />
+    </div>
   }
 };
