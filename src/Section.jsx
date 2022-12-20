d73eef5b6cda50e4d56be8a80a86ef728c6efd50
Iryna Furhala <irynafurhala@gmail.com>
2022-12-20 14:16:12 +0200

    create

---
+++ src/Section.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const Section = () => {
+    return <div>
+
+    </div>
+}
+
+Section.propTypes = {
+    title: PropTypes.string.isRequired
+}
